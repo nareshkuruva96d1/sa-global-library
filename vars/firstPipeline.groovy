7cfb2a6046d6f057d9fd2cfcd4cf1e7926715b7d
knaresh <knaresh@google.com>
2023-08-02 21:48:15 +0530

    updated

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,12 +1,11 @@
 import com.knrsacadamy.builds.Calculator
 def call (Map pipelineParams){
-    library('com.knrsacadamy.slb')
     Calculator calculator = new Calculator(this)
 
     pipeline {
         agent any
         environment {
-            APP_NAME = ${pipelineParams.appName}
+            APP_NAME = "${pipelineParams.appName}"
         }
         stages {
             stage('echo'){
