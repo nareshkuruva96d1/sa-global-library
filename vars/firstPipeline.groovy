62b67883943df7f17ad5c15753f368f6cdd7d020
nareshkuruva96d1 <135315223+nareshkuruva96d1@users.noreply.github.com>
2023-08-02 21:44:07 +0530

    Update firstPipeline.groovy

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,14 +1,21 @@
 import com.knrsacadamy.builds.Calculator
-pipeline {
-    agent any
-    stages {
-        stage('echo'){
-            steps {
-                scripts{
-                    echo "Printing sum of two numbers"
-                    println Calculator.add(3,4)
-                }
+def call (Map pipelineParams){
+    Calculator calculator = new Calculator(this)
+    pipeline {
+        agent any
+        environment {
+            APP_NAME = ${pipelineParams.appName}
+        }
+        stages {
+            stage('echo'){
+                steps {APP_NAME
+                    scripts{
+                        echo "Printing sum of two numbers"
+                        println calculator.add(3,4)
+                        echo "Microservice name is ${APP_NAME}"
+                    }
 
+                }
             }
         }
     }
