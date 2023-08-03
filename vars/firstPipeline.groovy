5be885d14d9319886d5db3ec8fd1176ef3b4a0fa
knaresh <knaresh@google.com>
2023-08-03 14:43:25 +0530

    numbers changed

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -12,9 +12,9 @@ def call (Map pipelineParams){
                 steps {
                     script {
                         echo "Printing sum of two numbers"
-                        println calculator.add(3,4)
+                        println calculator.add(4,2)
                         echo "Printing Multiple of two numbers"
-                        println calculator.multiply(3,4)
+                        println calculator.multiply(5,3)
                         echo "Microservice name is ${APP_NAME}"
                     }
                 }
